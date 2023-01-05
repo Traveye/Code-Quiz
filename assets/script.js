27a3020995164db06308bab5206827f525bd97a4
Big Trees <traveye@protonmail.com>
2023-01-04 19:15:56 -0800

    rewriting quizGo

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,45 @@
 
 // Timer Section triggered by beginQuiz function.
 
-var timerEl = document.querySelector(".timer")
+const timerEl = document.querySelector(".timer")
+totalTime = 60
+var timeLeft = 60
+const quizDone = false;
 
 
 function countDown () {
-    var timeLeft = 91
+    
     var timerInterval = setInterval(function(){
         timeLeft--;
         timerEl.textContent = timeLeft + " seconds remaining!";
-
-        quizGo();
         
 
         if(timeLeft === 0){
             clearInterval(timerInterval);
             sendMessage();
+            //add call to end game
         }
+
+        
 
     }, 1000);
 }
 
 function sendMessage() {
-    timerEl.textContent = "Times AHP"
+    timerEl.textContent = "Your time is up!"
 }
 
 // This section styles HTMl for pre quiz content.
 
-var titleEl = document.querySelector(".title")
-var questionEl = document.querySelector(".questions")
-var answerEl = document.querySelector(".answerField")
+const titleEl = document.querySelector(".title")
+const questionEl = document.querySelector(".questions")
+const answerEl = document.querySelector(".answerField")
+var score = 0;
 
 function beginQuiz () {
-    timerEl.textContent = "You will have 90 Seconds"
+    timerEl.textContent = "You will have " + totalTime + " seconds"
     titleEl.textContent = "Welcome to Code-Quiz!"
-    questionEl.textContent = "This quiz will present you wiht a series of quesitons to test your coding knowledge. Once you click start you will have 90 seconds to complete all the quesitons. But be careful! Any wrong answers will subtract 10 seconds from your remaining time! Good luck!"
+    questionEl.textContent = "This quiz will present you with a series of quesitons to test your coding knowledge. Once you click start you will have " + totalTime + " seconds to complete all the quesitons. But be careful! Any wrong answers will subtract 10 seconds from your remaining time! Good luck!"
     answerEl.style.display = "none";
    
     let btn = document.createElement("button");
@@ -42,70 +47,59 @@ function beginQuiz () {
     btn.className = "startBut"
     btn.onclick = function () {
         countDown();
+        quizGo();
         btn.style.display = "none"
+        
     
     }
     document.querySelector(".questionField").appendChild(btn);
-  
- 
+
+    
 }
 
 beginQuiz();
-// end pre quiz section
 
+// action 6 once all answers done or when time runs out - the game ends and score total is rendered
+// action 7 then you can enter you name / score in local storage and restart the game. 
 
-// Quiz Question array
+toAsk = ["Using _______ statement is how you test for a specific condition?", "How do you create a function in JavaScript?", "What JavaScript keyword declares a variable?"]
 
-questionsArr = [
-    "Using _______ statement is how you test for a specific condition?", "How do you create a function in JavaScript?", "What JavaScript keyword declares a variable?"
+userOptions = [
+    ["Select", "If", "Switch", "For"],
+
+    ["function:myFunction()", "function myFunction()", "function = myFunction()", "function is myFunction()"],
+
+    ["var", "if", "for", "create"]
 ]
 
-answersArr = {
-   question1: [["Select", false], ["If", true], ["Switch", false] ["For", true]],
-   question2: [[" function:myFunction()", false], [" function myFunction()", false], ["function = myFunction()", true], ["function is myFunction()", false]],
-   question3: [["var", true], ["if", false], ["for", false], ["create", false]]
-}
+var i = 0
+var j = 0
 
-answerKey = [
-    [false, true, false, true],
-    [false, false, true, false]
-    [true, false, false, false]
-]
-
-
-
-function quizGo () {
-    answerEl.style.display = "flex";
+function quizGo() {
+    //set display and options for first question
     titleEl.textContent = "Quiz Engaged"
-    questionEl.textContent = questionsArr[0];
-    document.querySelector("#answer1").textContent = answersArr[0][0];
-    document.querySelector("#answer2").textContent = answersArr[0][1];
-    document.querySelector("#answer3").textContent = answersArr[0][2];
-    document.querySelector("#answer4").textContent = answersArr[0][3];
-
-    // questions and options populated now onClick we need to check if correct
-
-    document.querySelectorAll('.button').forEach(function(button) {
-        button.addEventListener('click', function() {
-          console.log('Button was clicked');
-        });
+    answerEl.style.display = "flex"
+    questionEl.textContent = toAsk[i];
+    userOptions[j].forEach(option => {
+        const button = document.createElement('button');
+        button.textContent = option;
+        button.setAttribute('data-value', option);
+        button.addEventListener("click", checkAnswer)
+        answerEl.appendChild(button);
       });
-      
-
-    //sub funciont
-        //here we will add eevent listern for answer select
-        // if right increase score/ if wrong penalize time
-        // increment answer array and quesiton array
-
-
-
-
-function quizEnd () {
-
 }
 
-function highScore () {
-
+function checkAnswer() {
+    console.log("hey")
+    i++;
+    j++;
+    quizGo();
+    
 }
 
-}
+
+
+
+
+
+
